f8f4ae61455b81161297d77137cea5e7f15ac420
Sabra el hajjaji <selhajja@my.centennialcollege.ca>
2025-05-12 12:40:10 -0400

    git commit -m "Bind Flask to 0.0.0.0 for Render deployment"

--- server/app.py
+++ server/app.py
@@ -91,4 +91,7 @@ def update_task(title):
     return jsonify(message='Task updated'), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
