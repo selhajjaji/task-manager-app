d7b6929a6688b87f7bc2ade5a13a77c32a6fb982
Sabra el hajjaji <selhajja@my.centennialcollege.ca>
2025-05-12 13:11:10 -0400

    git commit -m "Render url"

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://task-manager-app-i17t.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 export default instance;
